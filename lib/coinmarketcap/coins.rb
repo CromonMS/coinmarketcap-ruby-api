1133db1b8c2b527bb7e40710f2c5fd0b23e63551
CromonMS <e@cromon.co.uk>
2015-10-26 22:24:27 +0000

    Better naming in coins.rb, all attributes now.

--- lib/coinmarketcap/coins.rb
+++ lib/coinmarketcap/coins.rb
@@ -4,25 +4,28 @@ require 'json'
 module Coinmarketcap
 	class Coins
 
-		attr_reader :count
+		attr_reader :count, :markets, :timestamp, :currency_exchange_rates
 
-		def initialize(coins)
-			@count = coins["markets"].count # @coins = Coinmarketcap::Coins.list(10) | @coins.count
+		def initialize(attributes)
+			@count = attributes["markets"].count # @coins = Coinmarketcap::Coins.list(10) | @coins.count
+			@markets = attributes["markets"]
+			@timestamp = attributes["timestamp"]
+			@currency_exchange_rates = attributes["currencyExchangeRates"]
+		end
+
+		# Coinmarketcap::Coins.all
+		def self.all
+			response = Faraday.get(BASE_URI + 'all.json')
+			attributes = JSON.parse(response.body)
+			new(attributes)
+			# attributes["markets"]
 		end
 
 		# Coinmarketcap::Coins.market_count
 		def self.market_count
 			response = Faraday.get(BASE_URI + 'all.json')
-			coins = JSON.parse(response.body)			
-			coins["markets"].count
+			attributes = JSON.parse(response.body)			
+			attributes["markets"].count
 		end
-
-		# Coinmarketcap::Coins.list
-		# accepts an integer in case you don't want all records
-		def self.list(count = self.market_count - 0)
-			response = Faraday.get(BASE_URI + 'all.json')
-			coins = JSON.parse(response.body)
-			all_coin_list = coins["markets"]
-		end	
 	end
 end
